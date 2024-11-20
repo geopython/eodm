bd538287d2eff5a6542b56904a98df5034e05612
jankovicgd <jankovic.gd@gmail.com>
2024-11-20 17:14:12 +0100

    feat: added extract concept

---
+++ src/eodm/extract.py
@@ -0,0 +1,50 @@
+from typing import Iterator
+
+import pystac_client
+from pystac import Collection, Item
+
+
+def extract_stac_api_items(
+    url: str,
+    collections: list[str] | None = None,
+    bbox: tuple[float, float, float, float] | None = None,
+    datetime_interval: str | None = None,
+    limit: int = 10,
+) -> Iterator[Item]:
+    """Extracts items from a STAC API
+
+    Args:
+        url (str): Link to STAC API endpoint
+        collections (list[str] | None, optional): List of collections to extract items from. Defaults to None.
+        bbox (tuple[float, float, float, float] | None, optional): Bounding box to search. Defaults to None.
+        datetime_interval (str | None, optional): Datetime interval to search. Defaults to None.
+        limit (int, optional): Limit query to given number. Defaults to 10.
+
+    Yields:
+        Iterator[Item]: pystac Items
+    """
+
+    client = pystac_client.Client.open(url)
+
+    search = client.search(
+        collections=collections,
+        bbox=bbox,
+        datetime=datetime_interval,
+        limit=limit,
+    )
+
+    yield from search.item_collection()
+
+
+def extract_stac_api_collections(url: str) -> Iterator[Collection]:
+    """Extracts collections from a STAC API
+
+    Args:
+        url (str): Link to STAC API endpoint
+
+    Yields:
+        Iterator[Collection]: pystac Collections
+    """
+
+    client = pystac_client.Client.open(url)
+    yield from client.get_collections()

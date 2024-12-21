56ec8450a72368f915969a218d9735a5bffcb4fe
jankovicgd <jankovic.gd@gmail.com>
2024-12-21 11:07:04 +0100

    test: added invalid tests

--- tests/test_opensearch.py
+++ tests/test_opensearch.py
@@ -1,6 +1,6 @@
 import pytest
 from data.test_data import OPENSEARCH_PRODUCT_TYPES
-from eodm.opensearch import OpenSearchClient
+from eodm.opensearch import OpenSearchClient, OpenSearchException
 
 
 @pytest.mark.parametrize("opensearch_product_type", OPENSEARCH_PRODUCT_TYPES)
@@ -14,3 +14,25 @@ def test_opensearch_search(
     query = {"{count}": 3, "{eo:productType}": opensearch_product_type}
     for i in client.search(query):
         assert i
+
+
+def test_opensearch_invalid_product_type(mock_opensearch_describe):
+    client = OpenSearchClient(
+        "https://finder.creodias.eu/resto/api/collections/describe.xml"
+    )
+
+    query = {"{count}": 3, "{eo:productType}": "INVALID"}
+    with pytest.raises(OpenSearchException):
+        for i in client.search(query):
+            i
+
+
+def test_opensearch_invalid_count(mock_opensearch_describe):
+    client = OpenSearchClient(
+        "https://finder.creodias.eu/resto/api/collections/describe.xml"
+    )
+
+    query = {"{count}": -5, "{eo:productType}": OPENSEARCH_PRODUCT_TYPES[0]}
+    with pytest.raises(OpenSearchException):
+        for i in client.search(query):
+            i
